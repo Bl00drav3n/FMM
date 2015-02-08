3eab6482af4f5a0f3f2a5e970987e8afb4d33b13
Bl00drav3n <bl00d_rav3n@gmx.net>
2015-02-08 04:43:54 +0100

    Implemented the concept of periodic boundary conditions. The arbitrary truncation (get_min_diff) does not work for coulomb forces (the correction integral does not converge for 1/r). Instead we would have to do something like Ewald Summation, but because we will use FMM later, we will do multipole expansions of the periodic base cells.

--- src/fmm_sim.cpp
+++ src/fmm_sim.cpp
@@ -28,19 +28,21 @@ struct fmm_data
     v2 *positions;
     v2 *velocities;
     v2 *forces;
+    v2 half_dim;
+    v2 dim;
     uint32_t num_particles;
 };
 
 static struct fmm_data data;
 
-static inline uint32_t
+static uint32_t
 calculate_required_memsize(uint32_t num_particles)
 {
     uint32_t result = 3 * num_particles * sizeof(v2);
     return result;
 }
 
-static inline void
+static void
 reset_forces()
 {
     for(uint32_t i = 0; i < data.num_particles; i++) {
@@ -48,7 +50,48 @@ reset_forces()
     }
 }
 
-static inline void
+static inline v2
+canonicalize(v2 v)
+{
+    v2 result;
+    result.x = v.x - ((int32_t)(v.x / data.half_dim.x)) * data.dim.x;
+    result.y = v.y - ((int32_t)(v.y / data.half_dim.y)) * data.dim.y;
+    assert(result.x >= -data.half_dim.x &&
+            result.x <= data.half_dim.x);
+    assert(result.y >= -data.half_dim.y &&
+            result.y <= data.half_dim.y);
+    return result;
+}
+
+/* 
+ * TODO: try to speed up the function (intrinsic?)
+ * in general we don't want to call floorf
+ */
+static inline v2
+canonicalize_position(v2 pos)
+{
+    v2 result;
+    result.x = pos.x - 
+        (floorf(pos.x / data.dim.x)) * data.dim.x;
+    result.y = pos.y - 
+        (floorf(pos.y / data.dim.y)) * data.dim.y;
+    assert(result.x >= 0.f &&
+            result.x < data.dim.x);
+    assert(result.y >= 0.f &&
+            result.y < data.dim.y);
+    return result;
+}
+
+static inline v2
+get_min_diff(v2 a, v2 b)
+{
+    v2 result;
+    v2 diff = a - b;
+    result = canonicalize(diff);
+    return result;
+}
+
+static void
 calculate_forces()
 {
     /* O(n^2) approach */
@@ -56,7 +99,7 @@ calculate_forces()
         v2 cur_pos = data.positions[j];
         for(uint32_t i = j + 1; i < data.num_particles; i++) {
             v2 other_pos = data.positions[i];
-            v2 diff = cur_pos - other_pos;
+            v2 diff = get_min_diff(cur_pos, other_pos);
             float absForce = 1 / lengthSq(diff);
             v2 force = absForce * diff;
             data.forces[j] += force;
@@ -65,22 +108,23 @@ calculate_forces()
     }
 }
 
-static inline void
+static void
 integrate(float timeStep)
 {
     /* simple (inaccurate) integration */
     for(uint32_t i = 0; i < data.num_particles; i++) {
         v2 velocity = data.velocities[i];
         data.velocities[i] += timeStep * data.forces[i];
-        data.positions[i] += timeStep * velocity;
+        data.positions[i] = 
+            canonicalize_position(data.positions[i] + timeStep * velocity);
     }
 }
 
-static inline void
-initialize_particles(uint32_t num_particles_x, uint32_t num_particles_y)
+static void
+initialize_data(uint32_t num_particles_x, uint32_t num_particles_y)
 {
     /* initialize equidistant grid */
-    v2 min_corner = V2(-1, -1);
+    v2 min_corner = V2(0, 0);
     v2 max_corner = V2(1, 1);
     v2 dim = max_corner - min_corner;
     float dx = dim.x / num_particles_x;
@@ -94,6 +138,9 @@ initialize_particles(uint32_t num_particles_x, uint32_t num_particles_y)
             idx++;
         }
     }
+
+    data.dim = dim;
+    data.half_dim = 0.5f * dim;
 }
 
 static void
@@ -129,7 +176,7 @@ main(int argc, char *argv[])
     data.forces = data.velocities + num_particles;
     data.num_particles = num_particles;
     
-    initialize_particles(num_particles_x, num_particles_y);
+    initialize_data(num_particles_x, num_particles_y);
 
     for(uint32_t loop = 0; loop < nIntegrations; loop++) {
         reset_forces();
